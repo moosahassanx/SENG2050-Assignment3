75682ecf2c75cf603872d69261b5d702c5686b67
jdawg093 <49189518+jdawg093@users.noreply.github.com>
2020-05-23 14:34:59 +1000

    Wrote the User Beans

---
+++ WEB-INF/classes/users/Student.java
@@ -0,0 +1,42 @@
+public class Student extends User
+{
+    public Student()
+    {
+        studentNumber = "";
+        averageMark = 0;
+        name = "";
+        phoneNumber = 0;
+        role = "";
+    }
+
+    public Student(String name, int phoneNumber, String role, String studentNumber, float averageMark)
+    {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+        this.studentNumber = studentNumber;
+        this.averageMark = averageMark;
+    }
+
+    public void setStudentNumber(String studentNumber)
+    {
+        this.studentNumber = studentNumber;
+    }
+
+    public void setAverageMark(float averageMark)
+    {
+        this.averageMark = averageMark;
+    }
+
+    public String getStudentNumber()
+    {
+        return studentNumber;
+    }
+
+    public float getAverageMark()
+    {
+        return averageMark;
+    }
+    private String studentNumber;
+    private float averageMark;
+}
---
+++ WEB-INF/classes/users/User.java
@@ -0,0 +1,51 @@
+public class User
+{
+    public User()
+    {
+        name = "";
+        phoneNumber = 0;
+        role = "";
+    }
+
+    public User(String name, int phoneNumber, String role)
+    {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(int phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setRole(String role)
+    {
+        this.role = role;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int phoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public String getRole()
+    {
+        return role;
+    }
+
+
+    protected String name;
+    protected int phoneNumber;
+    protected String role;
+}

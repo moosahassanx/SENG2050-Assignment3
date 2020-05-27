9ac9b5c47ee9a1cd5a671e1bb93151768f571fe4
humey <humeey44@gmail.com>
2020-05-28 00:16:30 +1000

    Teacher.java
    File got deleted on my merge rip

---
+++ WEB-INF/classes/userpackage/Teacher.java
@@ -0,0 +1,32 @@
+package userpackage;
+
+public class Teacher extends User
+{
+    public Teacher()
+    {
+        staffNumber = "";
+        name = "";
+        phoneNumber = 0;
+        role = "";
+    }
+
+    public Teacher(String name, int phoneNumber, String role, String staffNumber)
+    {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+        this.staffNumber = staffNumber;
+    }
+
+    public void setStaffNumber(String staffNumber)
+    {
+        this.staffNumber = staffNumber;
+    }
+
+    public String getStaffNumber()
+    {
+        return staffNumber;
+    }
+
+    private String staffNumber;
+}

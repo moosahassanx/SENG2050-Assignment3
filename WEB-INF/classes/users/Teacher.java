43033793a4612643a4e98ea045e04f47cd2b751b
humey <humeey44@gmail.com>
2020-05-27 21:41:03 +1000

    Update Teacher.java
    Adding database conn

--- WEB-INF/classes/users/Teacher.java
+++ WEB-INF/classes/users/Teacher.java
@@ -1,3 +1,9 @@
+import java.sql.*;
+import java.io.IOException;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class Teacher extends User
 {
     public Teacher()
@@ -27,4 +33,41 @@ public class Teacher extends User
     }
 
     private String staffNumber;
+
+
+
+
+
+
+
+    public boolean getTeacherUser(String username, String password) throws NamingException, SQLException {
+
+        InitialContext ctx = new InitialContext();
+        // Path to the datasource, SENG_Assignment3 is the main folder, collabDB is the DB name
+        DataSource ds = (DataSource) ctx.lookup("java:comp/env/SENG_Assignment3/collabDB");
+        Connection conn = ds.getConnection();
+        Statement stmt = conn.createStatement();
+            // Selecting all data from the website_user table ** Note - only gives username/passwords
+            String query = "SELECT * from website_users";
+            ResultSet rs = stmt.executeQuery(query);
+            PreparedStatement ps = null;
+
+                // Moving the curser over each tuple
+                while(rs.next()){
+                    String user = rs.getString("username");
+                    
+                }
+
+
+
+
+
+
+     return true;
+    }
+
+
+
+
+
 }

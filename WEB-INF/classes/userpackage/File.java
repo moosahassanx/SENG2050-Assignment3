290bd847c88c9e32de910c6853797a4e928d351d
humey <humeey44@gmail.com>
2020-06-04 10:04:42 +1000

    Update File.java

--- WEB-INF/classes/userpackage/File.java
+++ WEB-INF/classes/userpackage/File.java
@@ -1,47 +1,6 @@
 package userpackage;
 
-<<<<<<< HEAD
-public class File {
-    
-    private String userUploaded;
-    private String fileName;
-    private String description;
 
-    public File(){
-
-    }
-
-    public File(String userUploaded, String fileName, String description){
-        this.userUploaded = userUploaded;
-        this.fileName = fileName;
-        this.description = description;
-    }
-
-    public String getUserUploaded(){
-        return userUploaded;
-    }
-
-    public String getFileName(){
-        return fileName;
-    }
-
-    public String getDescription(){
-        return description;
-    }
-
-    public void setUserUploaded(String userUploaded){
-        this.userUploaded = userUploaded;
-    }
-
-    public void setFileName(String fileName){
-        this.fileName = fileName;
-    }
-
-    public void setDescription(String description){
-        this.description = description;
-    }
-
-=======
 import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;

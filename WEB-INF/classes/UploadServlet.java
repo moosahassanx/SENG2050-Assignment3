6afdf6be82ea08890c160d2a7f94f9b58248613a
humey <humeey44@gmail.com>
2020-06-04 10:03:20 +1000

    Update UploadServlet.java

--- WEB-INF/classes/UploadServlet.java
+++ WEB-INF/classes/UploadServlet.java
@@ -21,7 +21,14 @@ public class UploadServlet extends HttpServlet {
     //    fileDao = new FileDAO();    //Opening a Database object conneection
     //}
 
+<<<<<<< HEAD
+            //User user = (User) session.getAttribute("user");
+            File uploadFile = new File();
 
+            List<File> file = uploadFile.getAllFiles(uploadFile);
+=======
+
+>>>>>>> 0e349f706dad87619926e02c2cbaaa923e98730a
 
 
 
@@ -40,6 +47,33 @@ public class UploadServlet extends HttpServlet {
         InputStream fileBytes = filePart.getInputStream();
         byte[] bytes = fileBytes.readAllBytes(); 
 
+<<<<<<< HEAD
+        System.out.println(fileDescription);
+        System.out.println(groupName);
+        System.out.println(userUploaded);
+        System.out.println(fileName);
+
+        // Creating the file object
+        File uploadFile = new File(); 
+        // Setting files parameters, loaded constructor wouldnt work?
+        uploadFile.setUserUploaded(userUploaded);
+        uploadFile.setDescription(fileDescription);
+        uploadFile.setFileName(fileName);
+        uploadFile.setFileData(bytes);
+        // Sending file to upload method
+            try{
+
+                uploadFile.uploadFile(bytes,userUploaded, fileDescription, fileName);
+                RequestDispatcher rd = request.getRequestDispatcher("files.jsp"); 
+                rd.forward(request, response);
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+=======
+>>>>>>> 0e349f706dad87619926e02c2cbaaa923e98730a
 
 
 
